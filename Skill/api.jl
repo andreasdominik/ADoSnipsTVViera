09cb0b1d7c3c291a397cd10fbdfae747c5e44264
andreasdominik <andreas.dominik@mni.thm.de>
2019-06-05 16:31:01 +0200

    api started

--- Skill/api.jl
+++ Skill/api.jl
@@ -2,3 +2,28 @@
 # API function goes here, to be called by the
 # skill-actions:
 #
+
+VIERA_SH = "$(Snips.getAppDir())/Skill/viera.sh"
+
+
+function switchTVon(ip, gpio)
+
+    Snips.setGPIO(gpio, :on)
+    sleep(4)
+    runVieraCmd(ip, "TV")
+end
+
+
+function switchTVoff(ip, gpio)
+
+    runVieraCmd(ip, "powerOff")
+    sleep(4)
+    Snips.setGPIO(gpio, :off)
+end
+
+
+function runVieraCmd(ip, cmd)
+
+    shellcmd = `$VIERA_SH $ip $cmd`
+    snips.tryrun(shellcmd, wait = true)
+end
