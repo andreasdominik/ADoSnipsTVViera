6e963f47c85b9b6ee28b08313882d16af670d797
andreas.dominik <andreas.dominik@mni.thm.de>
2020-03-22 10:34:37 +0100

    adapted to new template

--- Skill/config.jl
+++ Skill/config.jl
@@ -1,21 +1,31 @@
-
-# language settings:
-# 1) set LANG to "en", "de", "fr", etc.
-# 2) link the Dict with messages to the version with
-#    desired language as defined in languages.jl:
-#
-
-const LANG = Snips.getLanguage()
-
-# DO NOT CHANGE THE FOLLOWING 3 LINES UNLESS YOU KNOW
+# DO NOT CHANGE THE FOLLOWING LINES UNLESS YOU KNOW
 # WHAT YOU ARE DOING!
 # set CONTINUE_WO_HOTWORD to true to be able to chain
 # commands without need of a hotword in between:
 #
-const CONTINUE_WO_HOTWORD = true
+const CONTINUE_WO_HOTWORD = false
 const DEVELOPER_NAME = "andreasdominik"
 Snips.setDeveloperName(DEVELOPER_NAME)
 Snips.setModule(@__MODULE__)
+#
+# language settings:
+# Snips.LANG in QnD(Snips) is defined from susi.toml or set
+# to "en" if no susi.toml found.
+# This will override LANG by config.ini if a key "language"
+# is defined locally:
+#
+if Snips.isConfigValid(:language)
+    Snips.setLanguage(Snips.getConfig(:language))
+end
+# or LANG can be set manually here:
+# Snips.setLanguage("fr")
+#
+# set a local const with LANG:
+#
+const LANG = Snips.getLanguage()
+#
+# END OF DO-NOT-CHANGE.
+
 
 # Slots:
 # Name of slots to be extracted from intents:
